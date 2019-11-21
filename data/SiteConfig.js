198d8e5299584cae6c92ea9c2a29df159a56782d
Anthony Walker <aawalker238@gmail.com>
2019-11-21 13:21:22 -0800

    updating SiteConfig

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -3,7 +3,7 @@ const config = {
   siteTitleShort: "GA Starter", // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation.
   siteTitleAlt: "GatsbyJS Advanced Starter", // Alternative site title for SEO.
   siteLogo: "/logos/logo-1024.png", // Logo used for SEO and manifest.
-  siteUrl: "https://gatsby-advanced-starter-demo.netlify.com", // Domain of your website without pathPrefix.
+  siteUrl: "http://localhost:8000/", // Domain of your website without pathPrefix.
   pathPrefix: "/", // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
   siteDescription: "A GatsbyJS stater with Advanced design in mind.", // Website description used for RSS feeds/meta description tag.
   siteRss: "/rss.xml", // Path to the RSS file.
@@ -25,7 +25,7 @@ const config = {
   userLinks: [
     {
       label: "GitHub",
-      url: "https://github.com/Vagr9K/gatsby-advanced-starter",
+      url: "https://github.com/aawalker238/icanblogtoo",
       iconClassName: "fa fa-github"
     },
     {
